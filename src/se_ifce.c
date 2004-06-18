260832cee4d31af9babcb2c4e431fefae4edad20
baud <baud@4525493e-7705-40b1-a816-d608a930855b>
2004-06-18 09:37:28 +0000

    use "abandon" method for read mode fix creation of missing directories better error messages
    git-svn-id: https://svn.cern.ch/reps/lcgutil/gfal/trunk@776 4525493e-7705-40b1-a816-d608a930855b

--- src/se_ifce.c
+++ src/se_ifce.c
@@ -3,7 +3,7 @@
  */
 
 /*
- * @(#)$RCSfile: se_ifce.c,v $ $Revision: 1.1 $ $Date: 2004/05/26 08:07:48 $ CERN Jean-Philippe Baud
+ * @(#)$RCSfile: se_ifce.c,v $ $Revision: 1.2 $ $Date: 2004/06/18 09:37:28 $ CERN Jean-Philippe Baud
  */
 
 #include <sys/types.h>
@@ -13,28 +13,44 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include "gfal_api.h"
+#undef SOAP_FMAC3
+#define SOAP_FMAC3 static
+#undef SOAP_FMAC5
+#define SOAP_FMAC5 static
 #include "seH.h"
-#include "soapEdgSeWebserviceService.nsmap"
+#include "soapEdgSeWebserviceService+.nsmap"
 #ifdef GFAL_SECURE
 #include "cgsi_plugin.h"
 #endif
+#include "seC.c"
+#include "seClient.c"
+
+static int
+se_init (struct soap *soap, const char *surl, char **srm_endpoint)
+{
+	int flags;
+	char *sfn;
+
+	if (parsesurl (surl, srm_endpoint, &sfn) < 0)
+		return (-1);
+
+	soap_init (soap);
+	soap->namespaces = namespaces_se;
+#ifdef GFAL_SECURE
+	flags = CGSI_OPT_DISABLE_NAME_CHECK | CGSI_OPT_SSL_COMPATIBLE;
+	soap_register_plugin_arg (soap, client_cgsi_plugin, &flags);
+#endif
+	return (0);
+}
 
 se_deletesurl (const char *surl)
 {
-	int flags;
 	struct impl__deleteResponse out;
-	char *sfn;
 	struct soap soap;
 	char *srm_endpoint;
 
-	if (parsesurl (surl, &srm_endpoint, &sfn) < 0)
+	if (se_init (&soap, surl, &srm_endpoint) < 0)
 		return (-1);
-
-	soap_init (&soap);
-#ifdef GFAL_SECURE
-	flags = CGSI_OPT_DISABLE_NAME_CHECK | CGSI_OPT_SSL_COMPATIBLE;
-	soap_register_plugin_arg (&soap, client_cgsi_plugin, &flags);
-#endif
 
 	/* issue "delete" request */
 
@@ -52,28 +68,31 @@ se_deletesurl (const char *surl)
 
 se_mkdir (const char *dir)
 {
-	int flags;
 	struct impl__mkdirResponse out;
-	char *sfn;
+	int ret;
+	int sav_errno;
 	struct soap soap;
 	char *srm_endpoint;
 
-	if (parsesurl (dir, &srm_endpoint, &sfn) < 0)
+	if (se_init (&soap, dir, &srm_endpoint) < 0)
 		return (-1);
-
-	soap_init (&soap);
-#ifdef GFAL_SECURE
-	flags = CGSI_OPT_DISABLE_NAME_CHECK | CGSI_OPT_SSL_COMPATIBLE;
-	soap_register_plugin_arg (&soap, client_cgsi_plugin, &flags);
-#endif
 
 	/* issue "mkdir" request */
 
-	if (soap_call_impl__mkdir (&soap, srm_endpoint,
-	    "mkdir", (char *)dir + 6, &out)) {
-		soap_print_fault (&soap, stderr);
+	if ((ret = soap_call_impl__mkdir (&soap, srm_endpoint,
+	    "mkdir", (char *)dir + 6, &out))) {
+		if (ret == SOAP_FAULT || ret == SOAP_CLI_FAULT) {
+			if (strstr (soap.fault->faultstring, "does not exist"))
+				sav_errno = ENOENT;
+			else
+				sav_errno = ECOMM;
+		} else
+			sav_errno = ECOMM;
+		if (sav_errno == ECOMM)
+			soap_print_fault (&soap, stderr);
 		soap_end (&soap);
 		soap_done (&soap);
+		errno = sav_errno;
 		return (-1);
 	}
 	soap_end (&soap);
@@ -81,10 +100,50 @@ se_mkdir (const char *dir)
 	return (0);
 }
 
-char *
-se_turlfromsurl (const char *surl, char **protocols, int oflag, char **reqid)
+int
+se_makedirp (const char *surl)
 {
-	int flags;
+	int c;
+	char *lastslash = NULL;
+	char *p;
+	char *p1;
+	char *q;
+	char sav_surl[1104];
+	struct stat64 statbuf;
+
+	if (strlen (surl) >= sizeof(sav_surl)) {
+		errno = EINVAL;
+		return (-1);
+	}
+	strcpy (sav_surl, surl);
+	p1 = strchr (sav_surl + 6, '/');
+	p = strrchr (sav_surl + 6, '/');
+	while (p > p1) {
+		if (lastslash == NULL) lastslash = p;
+		*p = '\0';
+		c = se_getfilemd (sav_surl, &statbuf);
+		if (c == 0) {
+			*p = '/';
+			break;
+		}
+		if (errno != ENOENT)
+			return (c);
+		q = strrchr (sav_surl, '/');
+		*p = '/';
+		p = q;
+	}
+	c = 0;
+	while (c == 0 && (p = strchr (p + 1, '/')) && p <= lastslash) {
+		*p = '\0';
+		c = se_mkdir (sav_surl);
+		*p = '/';
+	}
+	return (c);
+}
+
+char *
+se_turlfromsurl (const char *surl, char **protocols, int oflag, int *reqid, int *fileid, char **token)
+{
 	int nbproto = 0;
 	struct impl__cacheResponse outg;
 	struct impl__createResponse outp;
@@ -93,21 +152,14 @@ se_turlfromsurl (const char *surl, char **protocols, int oflag, char **reqid)
 	struct ArrayOf_USCORE_xsd_USCORE_string protoarray;
 	int ret;
 	int sav_errno;
-	char *sfn;
 	struct soap soap;
 	char *srm_endpoint;
 	xsd__long zero = 0;
 
-	if (parsesurl (surl, &srm_endpoint, &sfn) < 0)
+	if (se_init (&soap, surl, &srm_endpoint) < 0)
 		return (NULL);
 
 	while (*protocols[nbproto]) nbproto++;
-
-	soap_init(&soap);
-#ifdef GFAL_SECURE
-	flags = CGSI_OPT_DISABLE_NAME_CHECK | CGSI_OPT_SSL_COMPATIBLE;
-	soap_register_plugin_arg (&soap, client_cgsi_plugin, &flags);
-#endif
 
 	/* issue "cache" or the "create" request */
 
@@ -125,39 +177,54 @@ se_turlfromsurl (const char *surl, char **protocols, int oflag, char **reqid)
 					sav_errno = ECOMM;
 			} else
 				sav_errno = ECOMM;
-			soap_print_fault (&soap, stderr);
+			if (errno == ECOMM)
+				soap_print_fault (&soap, stderr);
 			soap_end (&soap);
 			soap_done (&soap);
 			errno = sav_errno;
 			return (NULL);
 		}
-		if ((*reqid = strdup (outg._cacheReturn)) == NULL) {
+		if ((*token = strdup (outg._cacheReturn)) == NULL) {
 			soap_end (&soap);
 			soap_done (&soap);
 			return (NULL);
 		}
 	} else {
-		if (soap_call_impl__create (&soap, srm_endpoint, "create",
-		    (char *)surl + 6, zero, "x", 36000, &outp)) {
-			soap_print_fault (&soap, stderr);
+retry:
+		if ((ret = soap_call_impl__create (&soap, srm_endpoint, "create",
+		    (char *)surl + 6, zero, "x", 36000, &outp))) {
+			if (ret == SOAP_FAULT || ret == SOAP_CLI_FAULT) {
+				if (strstr (soap.fault->faultstring, "o such file"))
+					sav_errno = ENOENT;
+					if (se_makedirp (surl) == 0) goto retry;
+				else if (strstr (soap.fault->faultstring, "File exists"))
+					sav_errno = EEXIST;
+				else
+					sav_errno = ECOMM;
+			} else
+				sav_errno = ECOMM;
+			if (errno == ECOMM)
+				soap_print_fault (&soap, stderr);
 			soap_end (&soap);
 			soap_done (&soap);
-			errno = ECOMM;
+			errno = sav_errno;
 			return (NULL);
 		}
-		if ((*reqid = strdup (outp._createReturn)) == NULL) {
+		if ((*token = strdup (outp._createReturn)) == NULL) {
 			soap_end (&soap);
 			soap_done (&soap);
 			return (NULL);
 		}
 	}
-	if (soap_call_impl__getTurl (&soap, srm_endpoint, "getTurl", *reqid,
+	if (soap_call_impl__getTurl (&soap, srm_endpoint, "getTurl", *token,
 	    &protoarray, &outq)) {
 		soap_print_fault (&soap, stderr);
 		soap_end (&soap);
 		soap_done (&soap);
 		return (NULL);
 	}
+	*reqid = 0;
+	*fileid = 0;
 	p = strdup (outq._getTurlReturn);
 	soap_end (&soap);
 	soap_done (&soap);
@@ -167,7 +234,6 @@ se_turlfromsurl (const char *surl, char **protocols, int oflag, char **reqid)
 se_getfilemd (const char *surl, struct stat64 *statbuf)
 {
 	char *dp;
-	int flags;
 	struct group *gr;
 	int i;
 	struct impl__getMetadataResponse out;
@@ -176,18 +242,11 @@ se_getfilemd (const char *surl, struct stat64 *statbuf)
 	char *q;
 	int ret;
 	int sav_errno;
-	char *sfn;
 	struct soap soap;
 	char *srm_endpoint;
 
-	if (parsesurl (surl, &srm_endpoint, &sfn) < 0)
+	if (se_init (&soap, surl, &srm_endpoint) < 0)
 		return (-1);
-
-	soap_init (&soap);
-#ifdef GFAL_SECURE
-	flags = CGSI_OPT_DISABLE_NAME_CHECK | CGSI_OPT_SSL_COMPATIBLE;
-	soap_register_plugin_arg (&soap, client_cgsi_plugin, &flags);
-#endif
 
 	/* issue "getMetadata" request */
 
@@ -196,11 +255,14 @@ se_getfilemd (const char *surl, struct stat64 *statbuf)
 		if (ret == SOAP_FAULT || ret == SOAP_CLI_FAULT) {
 			if (strstr (soap.fault->faultstring, "does not exist"))
 				sav_errno = ENOENT;
+			else if (strstr (soap.fault->faultstring, "expired"))
+				sav_errno = EACCES;
 			else
 				sav_errno = ECOMM;
 		} else
 			sav_errno = ECOMM;
-		soap_print_fault (&soap, stderr);
+		if (sav_errno == ECOMM)
+			soap_print_fault (&soap, stderr);
 		soap_end (&soap);
 		soap_done (&soap);
 		errno = sav_errno;
@@ -229,35 +291,41 @@ se_getfilemd (const char *surl, struct stat64 *statbuf)
 	return (0);
 }
 
-se_set_xfer_done (char *surl, char *reqid, int oflag)
+se_set_xfer_done (const char *surl, int reqid, int fileid, char *token, int oflag)
 {
-	int flags;
-	struct impl__commitResponse out;
-	char *sfn;
+	struct impl__abandonResponse outa;
+	struct impl__commitResponse outc;
 	struct soap soap;
 	char *srm_endpoint;
 
-	if (parsesurl (surl, &srm_endpoint, &sfn) < 0)
+	if (se_init (&soap, surl, &srm_endpoint) < 0)
 		return (-1);
 
-	soap_init (&soap);
-#ifdef GFAL_SECURE
-	flags = CGSI_OPT_DISABLE_NAME_CHECK | CGSI_OPT_SSL_COMPATIBLE;
-	soap_register_plugin_arg (&soap, client_cgsi_plugin, &flags);
-#endif
-	if (soap_call_impl__commit (&soap, srm_endpoint,
-	    "commit", reqid, &out)) {
-		soap_print_fault (&soap, stderr);
-		soap_end (&soap);
-		soap_done (&soap);
-		return (-1);
+	if ((oflag & O_ACCMODE) == 0) {
+/*		not implemented yet 
+		if (soap_call_impl__abandon (&soap, srm_endpoint,
+		    "abandon", token, &outa)) {
+			soap_print_fault (&soap, stderr);
+			soap_end (&soap);
+			soap_done (&soap);
+			return (-1);
+		}
+*/
+	} else {
+		if (soap_call_impl__commit (&soap, srm_endpoint,
+		    "commit", token, &outc)) {
+			soap_print_fault (&soap, stderr);
+			soap_end (&soap);
+			soap_done (&soap);
+			return (-1);
+		}
 	}
 	soap_end (&soap);
 	soap_done (&soap);
 	return (0);
 }
 
-se_set_xfer_running (char *surl, char *reqid)
+se_set_xfer_running (const char *surl, int reqid, int fileid, char *token)
 {
 	return (0);
 }
