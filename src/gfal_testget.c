a37cd4c0f6d274df3b69c064feada23334439ad5
baud <baud@4525493e-7705-40b1-a816-d608a930855b>
2004-10-20 10:36:38 +0000

    Initial release
    git-svn-id: https://svn.cern.ch/reps/lcgutil/gfal/trunk@803 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/gfal_testget.c
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2004 by CERN
+ */
+
+/*
+ * @(#)$RCSfile: gfal_testget.c,v $ $Revision: 1.1 $ $Date: 2004/10/20 10:36:38 $ CERN Jean-Philippe Baud
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include "gfal_api.h"
+#define DEFPOLLINT 10
+
+main(argc, argv)
+int argc;
+char **argv;
+{
+	struct srm_filestatus *filestatuses;
+	int i;
+	int nbfiles;
+	int nbprotocols;
+	int nbreplies;
+	static char *protocols[] = {"rfio"};
+	int r = 0;
+	int reqid;
+	char **surls;
+
+	if (argc < 2) {
+		fprintf (stderr, "usage: %s SURLs\n", argv[0]);
+		exit (1);
+	}
+	nbfiles = argc - 1;
+	surls = &argv[1];
+
+	nbprotocols = sizeof(protocols) / sizeof(char *);
+
+	if ((nbreplies = srm_get (nbfiles, surls, nbprotocols, protocols,
+	    &reqid, NULL, &filestatuses)) < 0) {
+		perror ("srm_get");
+		exit (1);
+	}
+
+	/* process files as soon as they are "ready" */
+
+	while (1) {
+		for (i = 0; i < nbreplies; i++) {
+			if ((filestatuses+i)->status == 1) {	/* file ready */
+				/* process it if not yet done */
+			}
+			if ((filestatuses+i)->surl)
+				free ((filestatuses+i)->surl);
+			if ((filestatuses+i)->turl)
+				free ((filestatuses+i)->turl);
+		}
+		free (filestatuses);
+
+		/* if processing complete, break */ if (r) break;
+
+		sleep ((r++ == 0) ? 1 : DEFPOLLINT);
+		if ((nbreplies = srm_getstatus (nbfiles, surls, reqid, NULL,
+		    &filestatuses)) < 0) {
+			perror ("srm_getstatus");
+			exit (1);
+		}
+	}
+	exit (0);
+}
