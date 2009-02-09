ea5c10857bb4ce105e41f1e223fb0b6939f274b8
rmollon <rmollon@4525493e-7705-40b1-a816-d608a930855b>
2009-02-09 15:27:42 +0000

    o Modify the test to use gfal_turlsfromsurls (srm v1 and v2 compliant) instead of internal srm_get
    git-svn-id: https://svn.cern.ch/reps/lcgutil/gfal/trunk@1481 4525493e-7705-40b1-a816-d608a930855b

--- src/gfal_testget.c
+++ src/gfal_testget.c
@@ -3,13 +3,14 @@
  */
 
 /*
- * @(#)$RCSfile: gfal_testget.c,v $ $Revision: 1.5 $ $Date: 2008/05/08 13:16:36 $ CERN Jean-Philippe Baud
+ * @(#)$RCSfile: gfal_testget.c,v $ $Revision: 1.6 $ $Date: 2009/02/09 15:27:42 $ CERN Jean-Philippe Baud
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <errno.h>
 #include "gfal_api.h"
 #define DEFPOLLINT 10
 
@@ -17,54 +18,67 @@ main(argc, argv)
 int argc;
 char **argv;
 {
-	struct srm_filestatus *filestatuses;
-	int i;
-	int nbfiles;
-	int nbprotocols;
-	int nbreplies;
-	static char *protocols[] = {"rfio", "dcap", "gsiftp"};
-	int r = 0;
-	int reqid;
-	char **surls;
-	int num_done = 0;
+	gfal_request req = NULL;
+	gfal_internal gobj = NULL;
+	gfal_filestatus *filestatuses = NULL;
+	int sav_errno = 0, n = 0, i = 0, nberrors = 0;
+	static char *protos[] = {"rfio", "dcap", "gsiftp"};
 
 	if (argc < 2) {
 		fprintf (stderr, "usage: %s SURLs\n", argv[0]);
 		exit (1);
 	}
-	nbfiles = argc - 1;
-	surls = &argv[1];
 
-	nbprotocols = sizeof(protocols) / sizeof(char *);
+	gfal_set_verbose (0);
 
-	if ((nbreplies = srm_get (nbfiles, (const char **) surls, nbprotocols, protocols,
-	    &reqid, NULL, &filestatuses, 0)) < 0) {
-		perror ("srm_get");
+	if ((req = gfal_request_new ()) == NULL)
+		exit (1);
+	req->nbfiles = argc - 1;
+	req->surls = argv + 1;
+	req->protocols = protos;
+
+	if (gfal_init (req, &gobj, NULL, 0) < 0) {
+		sav_errno = errno;
+		free (req);
+		errno = sav_errno;
+		perror (argv[0]);
+		exit (1);
+	}
+	free (req);
+
+	if (gfal_turlsfromsurls (gobj, NULL, 0) < 0) {
+		sav_errno = errno;
+		gfal_internal_free (gobj);
+		errno = sav_errno;
+		perror (argv[0]);
 		exit (1);
 	}
 
-	while (1) {
-		for (i = 0; i < nbreplies; i++) {
-			if ((filestatuses+i)->status == 1) {	
-				/* file ready */
-				num_done++;
-				printf("SURL %s Ready - TURL: %s\n", (filestatuses+i)->surl, 
-								     (filestatuses+i)->turl);
-			} else if ((filestatuses+i)->status == 0) {
-				printf("SURL %s Pending\n", (filestatuses+i)->surl);
-			} else {
-				printf("SURL %s Failed\n", (filestatuses+i)->surl);
-			}
-		}
+	if ((n = gfal_get_results (gobj, &filestatuses)) < 0) {
+		sav_errno = errno;
+		gfal_internal_free (gobj);
+		errno = sav_errno;
+		perror (argv[0]);
+		exit (1);
+	}
 
-		/* if processing complete, break */ if (num_done == nbreplies) break;
+	if (filestatuses == NULL) {
+		fprintf (stderr, "%s: Internal error (memory corruption?)\n", argv[0]);
+		exit (1);
+	}
 
-		sleep ((r++ == 0) ? 1 : DEFPOLLINT);
-		if ((nbreplies = srm_getstatus (nbfiles, (const char **) surls, reqid, NULL,
-						&filestatuses, 0)) < 0) {
-			perror ("srm_getstatus");
-			exit (1);
+	for (i = 0; i < n; ++i) {
+		if (filestatuses[i].status == 0)
+			printf("SURL %s Ready - TURL: %s\n", filestatuses[i].surl, filestatuses[i].turl);
+		else {
+			++nberrors;
+			if (filestatuses[i].explanation)
+				printf("SURL %s Failed:\n%s\n", filestatuses[i].surl, filestatuses[i].explanation);
+			else
+				printf("SURL %s Failed:\n%s\n", filestatuses[i].surl, strerror (filestatuses[i].status));
 		}
 	}
-	exit (0);
+
+	gfal_internal_free (gobj);
+	exit (nberrors > 0 ? 1 : 0);
 }
