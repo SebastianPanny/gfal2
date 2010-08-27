d6d815f317b75632f6bd04ca74ee8b0957e8bf1b
molnarzs <molnarzs@4525493e-7705-40b1-a816-d608a930855b>
2010-08-27 18:47:56 +0000

    Certification tests imported into GFAL development tests.
    git-svn-id: https://svn.cern.ch/reps/lcgutil/gfal/trunk@2262 4525493e-7705-40b1-a816-d608a930855b

--- test/certification_tests/execute_tests.sh
+++ test/certification_tests/execute_tests.sh
@@ -1,23 +1,28 @@
-# Copyright (c) Members of the EGEE Collaboration. 2004-2008.
-# See http://www.eu-egee.org/partners/ for details on the copyright
-# holders.
-#
+#! /bin/bash
+# Copyright (c) Members of the EGEE Collaboration. 2004.
+# See http://www.eu-egee.org/partners/ for details on the copyright holders.
+# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+# 
 #     http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# Authors: Zsolt Molnar <zsolt.molnar@cern.ch, http://www.zsoltmolnar.hu>
-# Version info: $Id: Makefile.am,v 1.1 2009/10/08 15:32:39 molnarzs Exp $
-# Release: $Name:  $
 
-SUBDIRS=regression certification_tests
+# These tests are independent. You can comment out any of them if you want to execute a 
+# selected set only.
 
-AUTOMAKE_OPTIONS = foreign
+source ../setup.sh
+
+export LOGSLOCATION="/tmp"
+export GFAL_BASIC="yes"
+export GFAL_PYTHON_API="yes"
+export SE_HOST=$SE_ENDPOINT_DPM
+export VO=$LCG_GFAL_VO
+
+./GFAL-certtest 
