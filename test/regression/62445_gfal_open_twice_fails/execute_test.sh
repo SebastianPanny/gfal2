2d3f83d98db7545ab5da72d973e2713779139b0c
molnarzs <molnarzs@4525493e-7705-40b1-a816-d608a930855b>
2010-08-31 11:02:41 +0000

    Taking into accoung the csec threading.
    git-svn-id: https://svn.cern.ch/reps/lcgutil/gfal/trunk@2271 4525493e-7705-40b1-a816-d608a930855b

--- test/regression/62445_gfal_open_twice_fails/execute_test.sh
+++ test/regression/62445_gfal_open_twice_fails/execute_test.sh
@@ -32,6 +32,7 @@ LOCAL_TEST_FILE=file:///etc/group
 
 export CSEC_TRACEFILE=$PWD/csec.trace
 export CSEC_TRACE=1
+export CSEC_NOTHREAD=1
 
 export GFAL_OPEN_TWICE_FAILS_TEST_FILE=$GFAL_OPEN_TWICE_FAILS_TEST_DIR/test.dat
 
@@ -44,7 +45,6 @@ BUILD_DIR=../../../build
 pushd $BUILD_DIR/test/regression/62445_gfal_open_twice_fails &> /dev/null
 ./62445_gfal_open_twice_fails
 popd &> /dev/null
-
 lcg-del -l --verbose --nobdii -D srmv2 --vo $LCG_GFAL_VO $GFAL_OPEN_TWICE_FAILS_TEST_FILE
 lcg-del -l -d --verbose --nobdii -D srmv2 --vo $LCG_GFAL_VO $GFAL_OPEN_TWICE_FAILS_TEST_DIR
 
