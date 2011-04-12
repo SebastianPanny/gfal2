bd946f03cb56b5402cca90e953c84dcd358dd505
adevress <adevress@4525493e-7705-40b1-a816-d608a930855b>
2011-04-12 12:35:24 +0000

    register modifs to the build files
    git-svn-id: https://svn.cern.ch/reps/lcgutil/gfal/branches/gfal_2_0_dev@3204 4525493e-7705-40b1-a816-d608a930855b

--- src/common/mds/mds_ifce.c
+++ src/common/mds/mds_ifce.c
@@ -28,36 +28,39 @@
 #include <sys/time.h>
 #include <lber.h>
 #include <ldap.h>
-#include "gfal_api.h"
 #include "gfal_internals.h"
 #include "ServiceDiscovery.h"
 
 
 
-/* get BDII hostname and port number */
-/* THIS LOGIC IS IMPLEMENTED IN THE NEW SD LIBRARY */
+/** @brief  get BDII hostname and port number 
+* THIS LOGIC IS IMPLEMENTED IN THE NEW SD LIBRARY *
+* */
 get_bdii (char *bdii_server, int buflen, int *bdii_port, char *errbuf, int errbufsz)
 {
     return (-1);
 }
 
-/* get from the BDII the RLS endpoints */
-/*NOT IMPLEMENTED*/
+/** @brief get from the BDII the RLS endpoints 
+NOT IMPLEMENTED */
 get_rls_endpoints (char **lrc_endpoint, char **rmc_endpoint, char *errbuf, int errbufsz)
 {
 	return (-1);
 }
 
 
-/* get from the BDII the CE Accesspoint for a given SE */
+/** @brief get from the BDII the CE Accesspoint for a given SE 
+ * 
+ * */
 get_ce_ap (const char *host, char **ce_ap, char *errbuf, int errbufsz)
 {
 	return sd_get_ce_ap(host, ce_ap);
 }
 
 
-/* get from the BDII the endpoint for the LFC */
-
+/** @brief  get from the BDII the endpoint for the LFC 
+ * 
+ **/
 get_lfc_endpoint (char **lfc_endpoint, char *errbuf, int errbufsz)
 {
 	char **fqan = NULL;
@@ -87,17 +90,16 @@ get_storage_path (const char *host, const char *spacetokendesc, char **sa_path, 
 }
 
 
-/* get from the BDII the SE type (disk, srm_v1) */
-
+/** @brief get from the BDII the SE type (disk, srm_v1) 
+ * */
 get_se_types_and_endpoints (const char *host, char ***se_types, char ***se_endpoints, char *errbuf, int errbufsz)
 {
 	return sd_get_se_types_and_endpoints(host, se_types, se_endpoints);
 }
 
-/* get from the BDII the list of supported protocols with their associated
+/** get from the BDII the list of supported protocols with their associated
  * port number
  */
-
 get_seap_info (const char *host, char ***access_protocol, int **port, char *errbuf, int errbufsz)
 {
 	return sd_get_seap_info(host, access_protocol, port);
