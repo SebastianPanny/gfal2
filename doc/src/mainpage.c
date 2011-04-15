3cea23d94ec2f49a88a4f691a8404e64c9de97f5
adevress <adevress@4525493e-7705-40b1-a816-d608a930855b>
2011-04-15 07:06:08 +0000

    new doc generation
    git-svn-id: https://svn.cern.ch/reps/lcgutil/gfal/branches/gfal_2_0_dev@3228 4525493e-7705-40b1-a816-d608a930855b

--- doc/src/mainpage.c
+++ doc/src/mainpage.c
@@ -1,7 +1,7 @@
 /**
 	\mainpage Gfal Documentation
 	
-	\author Devresse Adrien
+	\author Devresse Adrien ( adev88@gmail.com )
 	
 	<h1> SUMMARY : </h1>
 	
@@ -11,11 +11,14 @@
 	<h2>II. How to compile </h2>
 	- Compile :
 		- follow gfal hello world tutorial to have an etics installation
-		- cd CERNgfal
-		- ./myautogen.sh
+		- Install scons ( http://www.scons.org, python script )
+		- checkout the source from the svn : git svn clone http://svnweb.cern.ch/guest/lcgutil/gfal
+		- "cd CERNgfal"
+		- "scons "
+		- configure env vars ETICS_WORKSPACE if asked.
 	- Compile tests :
 		- Install python >=2.4
-		- Install scons ( http://www.scons.org )
+		- Install scons 
 		- execute "scons testing" in the CERNgfal dir
 	- Clean builds :
 		- scons -c
