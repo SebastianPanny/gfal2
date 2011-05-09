e3ba2ab20dfa018cbe912590ecac234ed88b11d1
adevress <adevress@4525493e-7705-40b1-a816-d608a930855b>
2011-05-09 12:22:18 +0000

    add initial posix access func test
    git-svn-id: https://svn.cern.ch/reps/lcgutil/gfal/branches/gfal_2_0_dev@3398 4525493e-7705-40b1-a816-d608a930855b

---
+++ test/posix/test__gfal_posix_access.c
@@ -0,0 +1,10 @@
+
+/* unit test for posix access func */
+
+
+#include <check.h>
+#include "gfal_constants.h"
+#include "gfal_prototypes.h"
+#include "gfal_types.h"
+#include "../unit_test_constants.h"
+
