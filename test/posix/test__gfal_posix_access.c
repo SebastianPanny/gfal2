09e628e41a780d30dcd5eb7ad72093391e7d8068
adevress <adevress@4525493e-7705-40b1-a816-d608a930855b>
2011-05-09 12:22:27 +0000

    add minor modif on the guid exist test
    git-svn-id: https://svn.cern.ch/reps/lcgutil/gfal/branches/gfal_2_0_dev@3403 4525493e-7705-40b1-a816-d608a930855b

--- test/posix/test__gfal_posix_access.c
+++ test/posix/test__gfal_posix_access.c
@@ -17,25 +17,25 @@ START_TEST(test_access_posix_guid_exist)
 {
 	// test exist guid
 	int ret;
-	ret = gfal_access("guid:ae571282-81ea-41af-ac3c-8b1a084bfc82", F_OK);
+
+	ret = gfal_access(TEST_GUID_VALID_ACCESS, F_OK);
 	if(ret != 0){
-		fail(" must be a valid access to the guid");
-		gfal_posix_print_error();
+		fail(" must be a valid access to the guid %s", strerror(ret));
+		gfal_posix_release_error();
 		return;
 	}
-	ret = gfal_access("guid:ae595782-81be-40af-ac3c-8b1a084bfc82", F_OK);
+	ret = gfal_access(TEST_GUID_NOEXIST_ACCESS, F_OK);
 	if(ret != ENOENT){
-		fail(" must be a non exist guid");
-		gfal_posix_print_error();
+		fail("must be a non exist guid %s", strerror(ret));
+		gfal_posix_release_error();
 		return;
-	}
-	
-	ret = gfal_access("google.com", F_OK);
-	if(ret != EINVAL){
-		fail(" must be a syntax error");		
-		gfal_posix_print_error();
 	}
 
-	
+	ret = gfal_access("google.com", F_OK);
+	if(ret != EINVAL){
+		fail(" must be a syntax error %s", strerror(ret));		
+		gfal_posix_release_error();
+	}
+
 }
 END_TEST
