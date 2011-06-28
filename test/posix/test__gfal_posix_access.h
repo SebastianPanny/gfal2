9bb8e6a81c09c1ea78750b33e16ae598568c51bc
adevress <adevress@4525493e-7705-40b1-a816-d608a930855b>
2011-06-28 15:04:15 +0000

    add forgotten files for the mocks
    git-svn-id: https://svn.cern.ch/reps/lcgutil/gfal/branches/gfal_2_0_dev@3677 4525493e-7705-40b1-a816-d608a930855b

---
+++ test/posix/test__gfal_posix_access.h
@@ -0,0 +1,18 @@
+#pragma once
+/*
+
+*	auto-generated header file for file test/posix/test__gfal_posix_access.c 
+ 
+*/
+
+
+ void test_access_posix_guid_exist();
+void test_access_posix_guid_read();
+void test_access_posix_guid_write();
+void test_access_posix_lfn_exist();
+void test_access_posix_lfn_read();
+void test_access_posix_lfn_write();
+void test_access_posix_srm_exist();
+void test_access_posix_srm_read();
+void test_access_posix_srm_write(); 
+
---
+++ test/posix/test__gfal_posix_chmod.h
@@ -0,0 +1,12 @@
+/*
+
+*	auto-generated header file for file test/posix/test__gfal_posix_chmod.c 
+ 
+*/
+
+
+ void test__gfal_posix_chmod_read_lfn();
+void test__gfal_posix_chmod_read_guid();
+void test__gfal_posix_chmod_read_local();
+void test__gfal_posix_chmod_write_lfn();
+
