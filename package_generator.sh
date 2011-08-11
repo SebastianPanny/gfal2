bd40b302c0438cab01c94637b6be68430661a68f
adevress <adevress@4525493e-7705-40b1-a816-d608a930855b>
2011-08-11 17:35:45 +0000

    modify package script for etics
    git-svn-id: https://svn.cern.ch/reps/lcgutil/gfal/branches/gfal_2_0_main@3876 4525493e-7705-40b1-a816-d608a930855b

--- package_generator.sh
+++ package_generator.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-
-
 package_list=(main main_devel doc meta dcap srm lfc rfio)
+
+if [ -z "$SCONS_EXE" ]
+then
+	SCONS_EXE="scons"
+fi
+
 
 mkdir -p RPMS
 for i in ${package_list[@]}
 do
-scons -j 8 package=$i "$@"
+$SCONS_EXE -j 8 package=$i "$@"
 if [ "$?" != "0" ]
   then
 	exit $?
