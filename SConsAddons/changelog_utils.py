51ee3e138747d06530e2a1e7275de0ad0d8ec192
adevress <adevress@4525493e-7705-40b1-a816-d608a930855b>
2012-01-25 07:47:57 +0000

    -remvoe conflicting files
    git-svn-id: https://svn.cern.ch/reps/lcgutil/gfal/branches/gfal_2_0_main@4838 4525493e-7705-40b1-a816-d608a930855b

--- SConsAddons/changelog_utils.py
+++
@@ -1,8 +0,0 @@
-# -*- conding: utf-8 -*-
-
-
-def get_rpm_changelog_from_file(filename = "CHANGELOG"):
-	f= open(filename, 'r')
-	d = f.readlines()
-	f.close()
-	return ''.join(d)
--- SConsAddons/symlink.py
+++
@@ -1,8 +0,0 @@
-##
-# builder for symlink creation
-
-from SCons import Builder
-
-def add_symlink(env):
-	lib_builder = Builder.Builder(action = "cd ${SOURCE.dir} && ln -s ${SOURCE.file} ${TARGET.file}", chdir = False)
-	env.Append(BUILDERS = {"LibrarySymlink" : lib_builder})
