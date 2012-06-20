8a0f11207d7eca892e06609e1a445e3fe5531707
adevress <adevress@4525493e-7705-40b1-a816-d608a930855b>
2012-06-20 10:04:34 +0000

    - add forgotten directory for file non-posix operations ( checksum )
    git-svn-id: https://svn.cern.ch/reps/lcgutil/gfal2/branches/devel_main@5431 4525493e-7705-40b1-a816-d608a930855b

---
+++ build/src/file/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/didi/workspace/CERN/gfal2/src/file
+# Build directory: /home/didi/workspace/CERN/gfal2/build/src/file
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
