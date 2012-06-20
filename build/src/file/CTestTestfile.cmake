25730440dd4c566f8e936d8a5777140d21191f55
adevress <adevress@4525493e-7705-40b1-a816-d608a930855b>
2012-06-20 10:06:17 +0000

    - correct add directory inclusion
    git-svn-id: https://svn.cern.ch/reps/lcgutil/gfal2/branches/devel_main@5432 4525493e-7705-40b1-a816-d608a930855b

--- build/src/file/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/didi/workspace/CERN/gfal2/src/file
-# Build directory: /home/didi/workspace/CERN/gfal2/build/src/file
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
